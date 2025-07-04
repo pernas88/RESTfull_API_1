071b35fe7253c18bcb3028e23fc5bdd6820f20ff
pernas88 <gabipernas88@gmail.com>
2025-07-04 11:39:20 +0100

    add app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer( (req, res) => { 
-
-    res.setHeader("Content-Type", "text/html; chartset=utf-8");
-    res.end("<h1> Hola mundo </h1>");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html; chartset=utf-8");
+  res.end("<h1> Hola mundo Gabi Pernas </h1>");
 });
 
-server.listen(PORT, () =>{
-    console.log(`Server running at http:localhost:${PORT}`)
+server.listen(PORT, () => {
+  console.log(`Server running at http:localhost:${PORT}`);
 });
